3b127e47d6878ebd036443bd48dcb0cb29da23ae
V3NCY <venceslavam@gmail.com>
2021-05-20 20:51:50 +0300

    constants fixed

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 export default {
-    imgPath: "https://res.cloudinary.com/plovdiv-university/image/upload/v1620560746/koprivshtitsa-hotels/"
+    imgPath: "https://res.cloudinary.com/plovdiv-university/image/upload/v1620496578/hotels-images/"
 }
